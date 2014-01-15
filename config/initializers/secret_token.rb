957aa47108d422317322b7c7c8a8bcee3caa7321
Sébastien Pencréach <pencreach.sebastien@gmail.com>
2014-01-15 21:04:55 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App365Films::Application.config.secret_key_base = 'c84f4c5c5aaf31f0a52fde2ad35293ac3ba584a335ff6e14e8166257e0f9181816685bd745bc139b39bc9596c52de73bd155eae0f8e5ae835bce7527a4223301'
